f29de27e8cfad97e480ef643d3d5390d97c48974
Claudio Bruno Martins <claudio.bruno.martins@gmail.com>
2010-11-28 21:11:04 -0200

    Upload de imagens

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,4 +2,8 @@ class Animal < ActiveRecord::Base
   belongs_to :usuario, :class_name => 'User'
   belongs_to :tipo
   belongs_to :raca
+  
+  has_attached_file :photo, :styles => { :small => "160x150#" },
+                    :url  => "/assets/animals/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/animals/:id/:style/:basename.:extension"
 end
